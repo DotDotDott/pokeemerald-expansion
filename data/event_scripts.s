d23dc1518febec0e66a65a9e74fff3ce07f94cbb
DotDotDott <pokitmonster@gmail.com>
2026-01-21 06:01:35 -0500

    added quest menu!
    added the quest menu but the colors are a bit fucked
    also lost the lights on windows being correct but i'll fix that later since it changes a lot

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -64,6 +64,7 @@
 #include "constants/union_room.h"
 #include "constants/vars.h"
 #include "constants/weather.h"
+#include "constants/quests.h"
 #include "constants/mugshots.h"
 #include "constants/speaker_names.h"
 	.include "asm/macros.inc"
