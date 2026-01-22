bbf0256b4dcf72e6dca877d46c233d0553fa4f1b
DotDotDott <pokitmonster@gmail.com>
2026-01-22 04:02:13 -0500

    added a few script additions
    big map thing, birds on roofs, skip large dex gaps i think.

--- src/field_specials.c
+++ src/field_specials.c
@@ -73,6 +73,9 @@
 #include "palette.h"
 #include "battle_util.h"
 #include "naming_screen.h"
+#include "constants/trainer_types.h"
+#include "battle_pyramid.h"
+#include "trainer_hill.h"
 
 #define TAG_ITEM_ICON 5500
 
@@ -4380,4 +4383,74 @@ void SetAbility(void)
 {
     u32 ability = gSpecialVar_Result;
     SetMonData(&gPlayerParty[gSpecialVar_0x8004], MON_DATA_ABILITY_NUM, &ability);
+}
+
+#define MAX_BIRD_SPOTS 10
+
+static const u8 randomBirdArray[MAX_BIRD_SPOTS + 1][4] =
+{
+    [0]  = { 0, 0, 0, 0 },
+    [1]  = { 1, 0, 1, 0 },
+    [2]  = { 1, 0, 1, 0 },
+    [3]  = { 2, 1, 1, 0 },
+    [4]  = { 2, 1, 1, 0 },
+    [5]  = { 2, 1, 2, 0 },
+    [6]  = { 3, 2, 2, 1 },
+    [7]  = { 3, 2, 3, 2 },
+    [8]  = { 4, 2, 3, 2 },
+    [9]  = { 4, 3, 3, 2 },
+    [10] = { 4, 3, 4, 3 },
+};
+
+void SetRoofBirds(void)
+{
+    u8 i;
+    u8 objectEventCount;
+    struct ObjectEventTemplate *template;
+    u8 birdIndexes[MAX_BIRD_SPOTS + 1] = {0};
+    u32 birdCount = 0;
+    if (gMapHeader.events != NULL)
+    {
+        if (InBattlePyramid_())
+           objectEventCount = GetNumBattlePyramidObjectEvents();
+        else if (InTrainerHill())
+           objectEventCount = HILL_TRAINERS_PER_FLOOR;
+        else
+           objectEventCount = gMapHeader.events->objectEventCount;
+
+        for (i = 0; i < objectEventCount; i++)
+        {
+            template = &gSaveBlock1Ptr->objectEventTemplates[i];
+            if (template->trainerType == TRAINER_TYPE_BIRD && birdCount <= MAX_BIRD_SPOTS)
+            {
+                birdIndexes[birdCount] = i; // Store index of bird
+                birdCount++;
+            }
+        }
+    
+        if (birdCount > 0)
+        {
+            u8 visibleCount = randomBirdArray[birdCount][Random() % 4];
+            Shuffle8(birdIndexes, birdCount);
+
+            // Mark the birds that will be visible as Seen in the Pokedex
+            for (i = 0; i < visibleCount; i++)
+            {
+                u8 birdIndex = birdIndexes[i];
+                template = &gSaveBlock1Ptr->objectEventTemplates[birdIndex];
+
+                // Find the SPECIES constant from the graphicsId
+                u32 speciesId = template->graphicsId - OBJ_EVENT_GFX_OW_MON;
+                GetSetPokedexFlag(speciesId, FLAG_SET_SEEN);
+            }
+
+            // Hide random birds until only `visibleCount` remain
+            for (i = visibleCount; i < birdCount; i++)
+            {
+                u8 birdIndex = birdIndexes[i];
+                template = &gSaveBlock1Ptr->objectEventTemplates[birdIndex];
+                FlagSet(template->flagId);
+            }
+        }
+    }
 }
